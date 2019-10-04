29092cf4043daa7f85c8b8dad6744327a22704d3
Mariel <mariel.code@gmail.com>
2019-10-04 15:03:00 +0000

    Create mapStateToProps function and add it to the export default connect method as an argument

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -6,10 +6,14 @@ class PostList extends React.Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
-  
+
   render () {
     return <div>Post List</div>;
   }
 }
 
-export default connect(null, { fetchPosts })(PostList);
+const mapStateToProps = (state) => {
+    return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);

cb3c7bf440040959d253d6c1b7e23c329d6f92d7
Mariel <mariel.code@gmail.com>
2019-10-04 16:25:52 +0000

    Created componentDidMount method

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -3,8 +3,12 @@ import { connect } from 'react-redux';
 import { fetchUser } from '../actions';
 
 class UserHeader extends React.Component {
+    componentDidMount() {
+			this.props.fetchUser(this.props.userId);
+    }
+
     render() {
-        return <div>User Header</div>;
+      return <div>User Header</div>;
     }
 }
 

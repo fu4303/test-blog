4d7dc3b6863d90a3f0c8c707d5e73f38853166c2
Mariel <mariel.code@gmail.com>
2019-10-04 16:23:28 +0000

    Import connect from react-redux and the newly created fetchUser action, then utilize the connect method at the bottom setting null as the first argument as we have not yet mapped state to props.

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
 
 class UserHeader extends React.Component {
     render() {
@@ -6,4 +8,4 @@ class UserHeader extends React.Component {
     }
 }
 
-export default UserHeader;
+export default connect(null, { fetchUser })(UserHeader);
